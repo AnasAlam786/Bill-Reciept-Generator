e7fe751a106c52184f71d30d759f698d64658d6c
AnasAlam786 <74352709+AnasAlam786@users.noreply.github.com>
2024-07-04 13:11:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import streamlit as st
 from CreatePDF import Reciept, FillCSV
 from datetime import datetime
 import pandas as pd
+import base64
 
-st.title("Reciept Generation")
+#st.title("Reciept Generation")
 AC = st.text_input("Account Number")
 name = st.text_input("Name")
 amount = st.text_input("Amount")
@@ -22,11 +23,22 @@ if st.button("Submit"):
   try:
     due=str(int(amount)-int(payed))
     Reciept(AC, name, amount,payed,due, str(time))
+
+    with open("receipt1.pdf", "rb") as pdf_file:
+      encoded_pdf = base64.b64encode(pdf_file.read()).decode('utf-8')
+
+    pdf_display = f'<iframe src="data:application/pdf;base64,{encoded_pdf}" width="710" height="480" type="application/pdf"></iframe>'
+
+    # Display the PDF in Streamlit
+    st.markdown(pdf_display, unsafe_allow_html=True)
     FillCSV(AC, name, amount,payed,due, str(time),"data.csv")
-    st.success("Reciept Generated")
+    #st.success("Reciept Generated")
 
   except Exception as e:
     st.error(f"Reciept Not Generated! Error = {e}",icon="🚨")
 
 df = pd.read_csv('data.csv')
 st.dataframe(df, use_container_width=True)
+
+
+
